2695ad3220f4c129573f37bb90df850eb965e092
anderson <andersion.v@gmail.com>
2016-09-27 13:00:56 +0800

    Add click listener in site activity.

--- app/src/main/java/cn/peacesky/beenews/ui/activity/menu/SiteActivity.java
+++ app/src/main/java/cn/peacesky/beenews/ui/activity/menu/SiteActivity.java
@@ -1,5 +1,7 @@
 package cn.peacesky.beenews.ui.activity.menu;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
@@ -8,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -24,12 +27,16 @@ public class SiteActivity extends BaseActivity {
     Spinner spinner;
     @InjectView(R.id.result)
     TextView result;
+    @InjectView(R.id.goto_btn)
+    Button goto_btn;
 
     private ArrayAdapter<CharSequence> adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final int[] url_sufix = {9, 10, 4, 6, 5, 8, 3, 11, 7, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
 
         setContentView(R.layout.activity_site);
         ButterKnife.inject(this);
@@ -44,10 +51,21 @@ public class SiteActivity extends BaseActivity {
         spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemSelected(AdapterView<?> parent, View view, final int position, long id) {
 
                 result.setText("  您的选择是：" + ((TextView) view).getText() + "\n"
-                        + "这是第" + spinner.getSelectedItemPosition() + "个城市");
+                        + "这是第" + spinner.getSelectedItemPosition() + "个城市" + position);
+                goto_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String url = "http://xmgl.ahau.edu.cn/station/show_station.aspx?stationid="
+                                + url_sufix[position];
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                        intent.setData(Uri.parse(url));
+                        startActivity(intent);
+                    }
+                });
             }
 
             @Override
